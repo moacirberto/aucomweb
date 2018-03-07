35490a58af72a5c8257a62f669b89a7ae162005f
Moacir Bertolezo <moacirberto@gmail.com>
2018-03-07 07:59:45 -0300

    Projeto Criado

---
+++ src/main/java/com/bertolezo/aucom/AucomwebApplication.java
@@ -0,0 +1,12 @@
+package com.bertolezo.aucom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucomwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AucomwebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bertolezo/aucom/AucomwebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bertolezo.aucom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AucomwebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
