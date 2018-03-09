2667a44e67ef03e7247d2043eba19bff942220a0
Moacir Bertolezo <moacirberto@gmail.com>
2018-03-09 08:09:24 -0300

    Criando operacao de instanciacao

--- src/main/java/com/bertolezo/aucom/AucomwebApplication.java
+++ src/main/java/com/bertolezo/aucom/AucomwebApplication.java
@@ -1,12 +1,32 @@
 package com.bertolezo.aucom;
 
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AucomwebApplication {
+import com.bertolezo.aucom.domain.Categoria;
+import com.bertolezo.aucom.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AucomwebApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AucomwebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
 	}
 }
