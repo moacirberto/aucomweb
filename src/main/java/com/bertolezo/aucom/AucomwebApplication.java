1cd6e224291e45a24006ad2b37b29ce41bda8291
moacirberto <moacirberto@gmail.com>
2018-04-05 23:18:09 -0300

    Paginacao Categoria

--- src/main/java/com/bertolezo/aucom/AucomwebApplication.java
+++ src/main/java/com/bertolezo/aucom/AucomwebApplication.java
@@ -70,7 +70,12 @@ public class AucomwebApplication implements CommandLineRunner{
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
-		
+		Categoria cat3 = new Categoria(null,"Cama mesa e banho");
+		Categoria cat4 = new Categoria(null,"Eletrônicos");
+		Categoria cat5 = new Categoria(null,"Jardinagem");
+		Categoria cat6 = new Categoria(null,"Decoração");
+		Categoria cat7 = new Categoria(null,"Perfumaria");
+
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
 		Produto p3 = new Produto(null,"Mouse",80.00);
@@ -84,6 +89,12 @@ public class AucomwebApplication implements CommandLineRunner{
 		
 		categoriaRepository.save(cat1);
 		categoriaRepository.save(cat2);
+		categoriaRepository.save(cat3);
+		categoriaRepository.save(cat4);
+		categoriaRepository.save(cat5);
+		categoriaRepository.save(cat6);
+		categoriaRepository.save(cat7);
+		
 		produtoRepository.save(p1);
 		produtoRepository.save(p2);
 		produtoRepository.save(p3);
--- src/main/java/com/bertolezo/aucom/services/CategoriaService.java
+++ src/main/java/com/bertolezo/aucom/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.bertolezo.aucom.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bertolezo.aucom.domain.Categoria;
@@ -40,5 +44,15 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
+				orderby);
+		return repo.findAll(pageRequest);
 	}
 }
